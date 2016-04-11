e4ac41e7a9296eda3ad2de738079bd9e199af306
Charley Bodkin <charley@bodkin.me>
2016-04-11 10:58:32 -0700

    bunch of experimental changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-    $('header h1').circleType({
-        radius: 1000,
-        fitText:true,
-        fitTextSize:1.2
-    });
     $('header h2').fitText(2.5);
     $('#many-hats').circleType({
         radius: 1000,
